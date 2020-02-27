677d4e8b832cdea9bd44987b485ac8cd2148177b
xhq <651619994@qq.com>
2020-02-27 12:30:03 +0800

    fea:修改api

--- src/common/utils/api.js
+++ src/common/utils/api.js
@@ -1,7 +1,7 @@
 export default {
-  GET_TASK_PROCESS:'/common/queuer/getTaskProcess', //推荐队列查询
-  ADD_TASK_PROCESS:'common/queuer/addTaskProcess', //推荐队列添加
-  GET_ORDER_DETAIL:'/common/order/getOrderDetail', //查询订单明细
+  GET_TASK_PROCESS:'/api/queuer/getTaskProcess', //推荐队列查询
+  ADD_TASK_PROCESS:'/api/queuer/addTaskProcess', //推荐队列添加
+  GET_ORDER_DETAIL:'/api/orderProcess/getOrderProcess', //订单流程查询
+  GET_PRODUCT:'/api/product/getProduct', //获取产品信息
   UPLOAD_IDENTITY_PHOTOS:'/identity/uploadIdentityPhotos', //上传身份证正返面
-
 }
