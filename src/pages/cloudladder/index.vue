0a7ce6f0519af5b4648a787bdd2ca9a3ece0293d
xhq <651619994@qq.com>
2020-02-28 10:37:23 +0800

    fea:加上代码示例

--- src/pages/cloudladder/index.vue
+++ src/pages/cloudladder/index.vue
@@ -6,6 +6,7 @@
 
 <script>
   import Loading from '@/common/components/loading/index'
+  import {getTaskProcess} from '@utils/service';
   import {mapMutations} from 'vuex';
     export default {
         name: "index",
@@ -27,6 +28,17 @@
                 this.CLEAR_ACCESS_TOKEN();
                 this.CLEAR_API_GATEWAY();
             },
+            /**
+             * ajax 请求试例
+             * @returns {Promise<void>}
+             */
+            async testPost(){
+                this.$loading({message:'请求中'})  //loaing
+                let [err,data]=await getTaskProcess({});
+                if(err!==null){this.$clear();this.$toast(err||'系统错误'); return ;};
+                this.$clear(); //清楚loading
+                //TODO 这里处理正常逻辑
+            }
         },
         created() {
             this.clearDefaultParams();
